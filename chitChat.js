71cdc8dc0216ad54fbedf83c97ab2c20263def0d
ZachParris <zacharyjparris@gmail.com>
2016-03-21 17:52:04 -0500

    initial commit

---
+++ chitChat.js
@@ -0,0 +1,16 @@
+var Chatty = (function() {
+	var userInput = [""]
+
+
+}(Chatty));
+
+
+
+
+
+
+
+// One IIFE should accept an element id, and the user message, and then add the user's message - along with the delete button - to the specified parent element. Each message should be stored in a private array in this IIFE. This IIFE should expose a function to read all messages, and delete a single message.
+
+//function with a for loop
+

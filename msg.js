aba831a5daac5f007d50baf2fa2c82ff8b8df80a
Teriq Perry <Teriq_Perry@apples-MacBook-Pro.local>
2016-03-25 17:38:27 -0500

    input display working

--- msg.js
+++ msg.js
@@ -1,16 +1,17 @@
 var Chatty = (function(msgChatty) {
-  var input = document.getElementById("textBox");
-  var msgBase = {};
+  var inputMsg = document.getElementById("textBox");
   
-  function msgDisplay() {
-  	var msgGet = Chatty.getter();
-    var msgBase = input.value;
+  function msgDisplay(messages) {
+  	var msgGet = Chatty.getter(messages);
+    var msgBase = inputMsg.value;
     msgGet.push(msgBase);
+  	console.log("messages", msgGet)
     Chatty.myData(msgGet);
   }
 
-	input.addEventListener("keypress", function(event) {
+	inputMsg.addEventListener("keypress", function(event) {
     if (event.keyCode === 13) {
+    	console.log("event", event)
       msgDisplay();
     }
 	})

7ea88ae0daf631b3592cc2cbde576c0cdcfdac5f
ZachParris <zacharyjparris@gmail.com>
2016-03-24 20:33:20 -0500

    I made messages display in the DOM

--- xhr.js
+++ xhr.js
@@ -2,6 +2,13 @@ var Chatty = (function() {
 	var chatCall = [];
 
 	return {
+		getChat: function() {
+			return chatCall;
+		},
+
+		setChat: function(newMessage) {
+			chatCall.push({"message": newMessage});
+		},
 
 		loadChat: function() {
 
@@ -10,13 +17,13 @@ var Chatty = (function() {
 			chatter.addEventListener("load", function() {
 				chatCall = JSON.parse(this.responseText).responses;
 				// console.log("messages", chatCall);
-				Chatty.myData(chatCall);
+				Chatty.displayMessages(chatCall);
 			});
 
 			chatter.open("GET", "messages.json");
 			chatter.send();
 		}
 	}
-}())
+}());
 
 Chatty.loadChat();

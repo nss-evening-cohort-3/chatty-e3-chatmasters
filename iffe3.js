b3d60330df7557339effc8cc2de1e922db012b2b
utleroy <bryanhankins@me.com>
2016-03-25 20:35:40 -0500

    Changed iffe3 to contain delete functionality

--- iffe3.js
+++ iffe3.js
@@ -1,14 +1,32 @@
 "use strict";
 
 let Chatty = function(original) {
+let erase = document.getElementsByClassName("delete");
+let input = document.getElementById("input");
+let container = document.getElementById("container");
 
 let acceptedMsg = document.querySelector("body").addEventListener("click", function(event) {
-  if (event.target.tagName === "button") {
+  if (event.target.tagName === "acceptedMsg") {
 
-    function removeElement(Chatty) {
-    let element = document.getElementById(gone);
-    element.parentNode.removeChild(button);
+    function deleteBtn(event) {
+    let thisCard = event.target.parentElement;
+    container.removeChild(thisCard)
+}
+
+function deleteBtnEventListener() {
+	for(let i = 0; i < erase.length; i++) {
+		let gone = document.getElementsByClassName("delete")
+		   for( let j = 0; j < gone.length; j++)
+		   	gone[j].addEventListener("click", deleteBtn)
+	}
+}
+
+    // function removeElement(event) {
+    //   let thisElement = document.getElementById(gone);
+    // thisElement.parentNode.removeChild(thisElement);
   }
  };
 
 }();_
+
+

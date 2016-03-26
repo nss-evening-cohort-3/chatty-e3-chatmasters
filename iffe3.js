04004a9b91e8049934e4ac661606a71d8fd75b5e
utleroy <bryanhankins@me.com>
2016-03-25 20:53:36 -0500

    simplified delete functionality

--- iffe3.js
+++ iffe3.js
@@ -1,9 +1,7 @@
 "use strict";
 
-let Chatty = function(original) {
-let erase = document.getElementsByClassName("delete");
-let input = document.getElementById("input");
-let container = document.getElementById("container");
+let ByeChatty = function(original) {
+let erase = document.getElementsByClassName("gone");
 
 let acceptedMsg = document.querySelector("body").addEventListener("click", function(event) {
   if (event.target.tagName === "acceptedMsg") {
@@ -13,20 +11,17 @@ let acceptedMsg = document.querySelector("body").addEventListener("click", funct
     container.removeChild(thisCard)
 }
 
-function deleteBtnEventListener() {
+function deleteBtnEventListener(event) {
 	for(let i = 0; i < erase.length; i++) {
-		let gone = document.getElementsByClassName("delete")
-		   for( let j = 0; j < gone.length; j++)
-		   	gone[j].addEventListener("click", deleteBtn)
-	}
-}
+		let bye = document.getElementsByClassName("gone")
+		   for( let j = 0; j < bye.length; j++)
+		   	bye[j].addEventListener("click", Delete)
+	  }
+    }
 
-    // function removeElement(event) {
-    //   let thisElement = document.getElementById(gone);
-    // thisElement.parentNode.removeChild(thisElement);
-  }
- };
+   }
+ });
 
-}();_
+}();
 
 
